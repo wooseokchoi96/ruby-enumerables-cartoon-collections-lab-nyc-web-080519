b7f2ab8e32c51edda759fead79a55b47359d0bef
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 15:59:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,6 @@ end
 
 def find_the_cheese(my_array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if my_array_of_strings.include?(cheese_types.each)
+  if my_array_of_strings.include?(cheese_types.each) then
+    
 end
