c07f5e1648bd7c06cb53b2c9a347abef3b28483b
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 15:55:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(planeteer_calls)
   new
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(my_calls)
+  my_calls.any?
 end
 
 def find_the_cheese# code an argument here
