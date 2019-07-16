3b6c6e377283a4fc2eaf926712a5c102a8570576
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 15:32:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_array)
+  dwarves_array.sort.map{ |dwarf|
+    puts dwarf
+  }
 end
 
 def summon_captain_planet# code an argument here
