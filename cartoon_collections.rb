5cd4bb4fdd5fe19dd17cdc2074c2699f00c5530f
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 15:56:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(my_calls)
   my_calls.any?{ |word| word.length > 4 }
 end
 
-def find_the_cheese(my_array)
+def find_the_cheese(my_array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
