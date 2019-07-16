0561c811907558199fe048282f50302b9b263e60
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 15:56:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(my_calls)
-  my_calls.any?{ |word| word.length > 
+  my_calls.any?{ |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
